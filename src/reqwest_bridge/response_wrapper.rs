d6034cfb46961ee2fc0038f18552a5400b806557
chaosmo <chaosmo.zhu@mexc.com>
2024-12-24 21:55:13 +0800

    feat(project): finished gzip stream proxy

--- src/reqwest_bridge/response_wrapper.rs
+++ src/reqwest_bridge/response_wrapper.rs
@@ -1,6 +1,7 @@
 use actix_web::{HttpResponse, body::BoxBody, http::StatusCode};
 use async_compression::futures::bufread::GzipDecoder;
 use futures::{io::ErrorKind, prelude::*};
+use reqwest::header::HeaderMap;
 
 #[derive(Debug)]
 pub struct ResponseWrapper {
@@ -12,48 +13,70 @@ impl ResponseWrapper {
         Self { response }
     }
 
-    pub async fn into(self) -> HttpResponse {
-        if let Ok(response) = self.response {
-            let headers = response.headers().clone();
-            let status = response.status();
-            log::info!("headers: {:#?}", headers);
-            if status.is_success() {
-                let content_type_opt = headers.get("content-type");
-                let content_type = match content_type_opt {
-                    Some(content_type) => content_type.to_str().unwrap_or("application/text"),
-                    None => "application/text",
-                };
-                let content_encoding_opt = headers.get("content-encoding");
-                let content_encoding = match content_encoding_opt {
-                    Some(content_encoding) => content_encoding.to_str().unwrap_or(""),
-                    None => "",
-                };
-                let mut reader = response
-                    .bytes_stream()
-                    .map_err(|e| std::io::Error::new(ErrorKind::Other, e))
-                    .into_async_read();
-                let mut data = String::new();
-                if "gzip" == content_encoding || "application/gzip" == content_type {
-                    let mut decoder = GzipDecoder::new(reader);
-                    decoder.read_to_string(&mut data).await.unwrap();
-                } else {
-                    reader.read_to_string(&mut data).await.unwrap();
-                }
-                HttpResponse::new(StatusCode::from_u16(status.as_u16()).unwrap())
-                    .set_body(BoxBody::new(data))
-            } else {
-                HttpResponse::new(StatusCode::from_u16(status.as_u16()).unwrap())
-            }
+    async fn handle_reqwest_response(response: reqwest::Response) -> HttpResponse {
+        let headers = response.headers().clone();
+        let status = response.status();
+        log::info!("headers: {:#?}", headers);
+        if !status.is_success() {
+            log::info!("status: {}", status);
+            return HttpResponse::new(StatusCode::from_u16(status.as_u16()).unwrap());
+        }
+        Self::into_actix_response(headers, status, response).await
+    }
+
+    fn get_header(headers: Box<HeaderMap>, key: &str, default: &str) -> String {
+        let value_opt = headers.get(key);
+        let value = match value_opt {
+            Some(content_type) => content_type.to_str().unwrap_or(default),
+            None => default,
+        };
+        String::from(value)
+    }
+
+    async fn into_actix_response(
+        headers: HeaderMap,
+        status: reqwest::StatusCode,
+        response: reqwest::Response,
+    ) -> HttpResponse {
+        let headers = Box::new(headers);
+        let content_type = Self::get_header(headers.clone(), "content-type", "application/text");
+        let content_encoding = Self::get_header(headers.clone(), "content-encoding", "");
+        let mut reader = response
+            .bytes_stream()
+            .map_err(|e| std::io::Error::new(ErrorKind::Other, e))
+            .into_async_read();
+        let mut data = String::new();
+        if "gzip" == content_encoding || "application/gzip" == content_type {
+            let mut decoder = GzipDecoder::new(reader);
+            decoder.read_to_string(&mut data).await.unwrap();
         } else {
-            log::error!("response failed: {}", self.response.err().unwrap());
-            HttpResponse::InternalServerError().body("error")
+            reader.read_to_string(&mut data).await.unwrap();
+        }
+        let mut ret = HttpResponse::new(StatusCode::from_u16(status.as_u16()).unwrap())
+            .set_body(BoxBody::new(data));
+        let response_headers = ret.headers_mut();
+        headers.iter().for_each(|(key, value)| {
+            response_headers.insert(
+                key.as_str().parse().unwrap(),
+                value.to_str().unwrap().parse().unwrap(),
+            );
+        });
+        ret
+    }
+
+    pub async fn into(self) -> HttpResponse {
+        match self.response {
+            Err(err) => {
+                log::error!("request error: {}", err);
+                return HttpResponse::new(StatusCode::INTERNAL_SERVER_ERROR);
+            }
+            Ok(response) => Self::handle_reqwest_response(response).await,
         }
     }
 }
 
 #[cfg(test)]
 mod tests {
-    use std::io::{self, BufReader, ErrorKind};
 
     use actix_web::{HttpResponse, body::MessageBody, http::StatusCode};
     use http::response::Builder;
@@ -78,9 +101,9 @@ mod tests {
         let (http_response, body) = http_response.into_parts();
         let headers = http_response.headers();
         assert_eq!(http_response.status(), StatusCode::OK);
-        // assert_eq!(headers.get("Content-Type").unwrap(), "text/html");
-        // assert_eq!(headers.get("X-Custom-Foo").unwrap(), "bar");
-        // assert_eq!(headers.get("content-length").unwrap(), "0");
+        assert_eq!(headers.get("Content-Type").unwrap(), "text/html");
+        assert_eq!(headers.get("X-Custom-Foo").unwrap(), "bar");
+        assert_eq!(headers.get("content-length").unwrap(), "0");
         assert_eq!(body.try_into_bytes().unwrap(), "foo".as_bytes());
     }
 
